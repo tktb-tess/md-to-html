83a9c916b5c2bcde4373b5e5031e5751ac8fc927
Born-Bohrer <chem1000leaves@gmail.com>
2025-07-10 23:43:51 +0900

    aaaa

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,23 @@
+<script lang="ts">
+  import { marked } from 'marked';
+  let markdownText = $state('');
 
-<main>
-  <p>Hi!</p>
+  const parsedHtml = $derived.by(() => {
+    return marked.parse(markdownText, { async: false });
+  });
+</script>
+
+<main class="mx-2">
+  <h1 class="text-center my-8">Markdown to HTML</h1>
+  <div class="flex flex-col">
+    <div class="flex flex-col items-center gap-2">
+      <label for="input" class="text-xl font-serif">Markdown</label>
+      <textarea id="input" bind:value={markdownText}></textarea>
+    </div>
+    <p class="self-center text-xl">↓↓↓</p>
+    <div class="flex flex-col items-center gap-2">
+      <label for="output" class="text-xl font-serif">Parsed HTML</label>
+      <textarea id="output" readonly>{parsedHtml}</textarea>
+    </div>
+  </div>
 </main>
