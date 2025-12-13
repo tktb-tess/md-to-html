f5fe0cda931071be9f23824d75648f1d035e088f
tktb-tess <chemusiclove6174@gmail.com>
2025-12-13 12:50:51 +0900

    update, etc.

--- src/decl.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '@joplin/turndown' {
-  export { default } from 'turndown';
-}
-
-declare module '@joplin/turndown-plugin-gfm' {
-  import TurndownService from 'turndown';
-  export const gfm: (service: TurndownService) => void;
-}
--- src/lib/components/global.svelte.ts
+++ src/lib/components/global.svelte.ts
@@ -1,7 +1,6 @@
 import { SvelteMap } from 'svelte/reactivity';
 
-type UUID = ReturnType<typeof crypto.randomUUID>;
-type ToastType = 'info' | 'warning' | 'caution' | 'ok';
+export type ToastType = 'info' | 'warning' | 'caution' | 'ok';
 type Toast = {
   text: string;
   duration: number;
@@ -9,17 +8,19 @@ type Toast = {
   timeoutID: number;
 };
 
-export const toasts = new SvelteMap<UUID, Toast>();
+export const toasts = new SvelteMap<number, Toast>();
 
 export const addToast = (text: string, duration: number, type: ToastType) => {
-  const id = crypto.randomUUID();
+  const id = Date.now();
+
   const timeoutID = setTimeout(() => {
     dismissToast(id);
   }, duration);
+
   toasts.set(id, { text, duration, timeoutID, type });
 };
 
-export const dismissToast = (id: UUID) => {
+export const dismissToast = (id: number) => {
   const t = toasts.get(id);
   if (!t) return;
   clearTimeout(t.timeoutID);
